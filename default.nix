2ea5f23a664f6f106669aaabb7545d64e75c7657
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2019-02-01 15:24:34 +0100

    Support building with math-comp master.

--- default.nix
+++ default.nix
@@ -1,12 +1,22 @@
-{ pkgs ? (import <nixpkgs> {}), coq-version-or-url, shell ? false }:
+{ pkgs ? (import <nixpkgs> {})
+, coqPackagesInfo ? {
+    coq = "https://github.com/coq/coq/tarball/master";
+    ssreflect = "https://github.com/math-comp/math-comp/tarball/master";
+  }
+, shell ? false
+}:
 
-let
-  coq-version-parts = builtins.match "([0-9]+).([0-9]+)" coq-version-or-url;
-  coqPackages =
-    if coq-version-parts == null then
-      pkgs.mkCoqPackages (import (fetchTarball coq-version-or-url) {})
-    else
-      pkgs."coqPackages_${builtins.concatStringsSep "_" coq-version-parts}";
+let coqPackages =
+  if builtins.isString coqPackagesInfo then
+    let coq-version-parts = builtins.match "([0-9]+).([0-9]+)" coqPackagesInfo; in
+    pkgs."coqPackages_${builtins.concatStringsSep "_" coq-version-parts}"
+  else rec {
+    coq = import (fetchTarball coqPackagesInfo.coq) {};
+    ssreflect = pkgs.coqPackages.ssreflect.overrideAttrs (_: {
+      inherit coq;
+      src = fetchTarball coqPackagesInfo.ssreflect;
+    });
+  };
 in
 
 with coqPackages;
